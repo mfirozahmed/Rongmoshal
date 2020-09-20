032789097162cd208799ad974277bf13a029ff85
M. Firoz Ahmed <mfiroz.sust@gmail.com>
2020-09-21 03:06:39 +0600

    User controller updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,8 +21,84 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function profile()
     {
-        return view('home');
+        if (Auth::check()) {
+            $data = Code::categoryList();
+            $token_user = User::find(Auth::user()->id);
+            return view('frontend.user.profile')->with('token_user', $token_user)->with($data);
+        }
+        
+        Session::flash('error', 'Unauthorised Access Detected!');
+        return redirect()->route('home');
     }
+
+    public function profile_update()
+    {
+        if (Auth::check()) {
+            $data = Code::categoryList();
+            $token_user = User::find(Auth::user()->id);
+            return view('frontend.user.profile_update')->with('token_user', $token_user)->with($data);
+        }
+
+        Session::flash('error', 'Unauthorised Access Detected!');
+        return redirect()->route('home');
+    }
+
+    public function profile_submit(Request $request)
+    {
+        $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'address' => ['required'],
+            'phone' => ['required', 'string', 'size:11'],
+            'password' => 'required'
+        ],
+        [
+            'name.required' => 'Enter your name please.',
+            'address.required' => 'Enter your address please.',
+            'phone.required' => 'Enter a valid phone number please.',
+            'password.required' => 'Please Enter your password to proceed.'
+        ]);
+
+        if (Auth::check()) {
+            
+            $token_user = User::find(Auth::user()->id);
+
+            if (!(Hash::check($request->password, $token_user->password))){
+                Session::flash('error', 'Password does not match.');
+                return back();
+            }
+    
+            $token_user->name = $request->name;
+            $token_user->phone = $request->phone;
+            $token_user->address = $request->address;
+    
+            if ($request->new_password != null) {
+                if ($request->new_password == $request->confirm_password) {
+                    $token_user->password = Hash::make($request->new_password);
+                }
+            }
+            
+            $token_user->save();
+            Session::flash('success', 'Profile updated successfully.');
+            return redirect()->route('user.dashboard');
+        }
+
+        Session::flash('error', 'Unauthorised Access Detected!');
+        return redirect()->route('home');
+    }
+
+    public function order()
+    {
+        $data = Code::categoryList();
+        if (Auth::check()) {
+            $token_user = User::find(Auth::user()->id);
+            $orders = Order::where('user_id', $token_user->id)->get();
+        } else {
+            $orders = Order::where('user_id', request()->ip())->get();
+            
+        }
+        return view('frontend.user.order')->with('orders', $orders)->with($data);
+    }
+
 }
