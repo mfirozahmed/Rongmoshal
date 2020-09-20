62f31c0b32e211cd4af55572f6b42c914461a1ee
M. Firoz Ahmed <mfiroz.sust@gmail.com>
2020-09-21 03:18:04 +0600

    Admin forget password added

--- app/Http/Controllers/Auth/AdminLoginController.php
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -21,26 +21,40 @@ class AdminLoginController extends Controller
 
     public function login(Request $request)
     {
+        //Error messages
+        $messages = [
+            "email.required" => "Email is required",
+            "email.email" => "Email is not valid",
+            "email.exists" => "These credentials do not match our records.",
+            "password.required" => "Password is required",
+        ];
+
         //Validate the form data
 
-        $this->validate($request, [
+        /* $this->validate($request, [
             'email' => 'required',
             'password' => 'required'
-        ]);
+        ]); */
 
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|exists:admins',
+            'password' => 'required'
+        ], $messages);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
         //Attempt to log the user in
 
-        if (Auth::guard('admin')->attempt(
-            ['email' =>
-            $request->email, 'password' => $request->password],
-            $request->remember
-        )) {
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
             // successful-> redirect to intended location
 
             return redirect()->intended(route('admin.dashboard'));
         }
 
-        return redirect()->back()->withInput($request->only('email', 'remember'));
+        return redirect()->back()->withInput($request->only('email', 'remember'))->withErrors([
+            'approve' => 'Wrong password or this account is not approved yet.',
+        ]);
     }
 
     public function adminLogout()
